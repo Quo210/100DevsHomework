bbdca5884e6b1d0815438f2954783e7565d65e5d
Quo210 <gabrielrengifo211@gmail.com>
2022-05-23 22:58:17 -0400

    daily practice

--- codeWars/scripts.js
+++ codeWars/scripts.js
@@ -303,3 +303,11 @@ function basicOp(operation, value1, value2)
   }
 }
 
+//8
+
+function hoopCount (n) {
+  return (n >= 10)? 'Great, now move on to tricks' : "Keep at it until you get it"    
+}
+
+
+
