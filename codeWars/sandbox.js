e6c4186b51f5705972a3cd01b68af16f6dda0d41
Quo210 <gabrielrengifo211@gmail.com>
2022-06-24 20:49:45 -0400

    daily

--- codeWars/sandbox.js
+++ codeWars/sandbox.js
@@ -1,9 +1,3 @@
-function rowSumOddNumbers(n) {
-  let pyramidLevel = 0;
-  let lastInt;
-  let endResult = 0;
-  while (pyramidLevel != n){
-    pyramidLevel++
-    for (let innerCounter = 0; innerCounter < pyramidLevel; innerCounter++)
-  }
+if (true){
+  console.log('forgetful lettuce')
 }
