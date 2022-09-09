3f2dfd7b6b41bb60815ae149bb0e892bcfb532fc
Quo210 <gabrielrengifo211@gmail.com>
2022-09-09 06:00:09 -0400

    9

--- codeWars/September/practice.js
+++ codeWars/September/practice.js
@@ -1,3 +1,14 @@
 function removeUrlAnchor(url){
     return url.split('#')[0];
   }
+
+  const sequenceSum = (begin, end, step) => {
+    if(begin > end) return 0;
+    const arr = [];
+    let accum = begin;
+    while (accum <= end){
+      arr.push(accum)
+      accum += step
+    }
+    return  arr.reduce((a,b)=>a+b)
+  };
